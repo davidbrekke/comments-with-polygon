83afb143b22706f914ac6fe79bea67d90cac72c5
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-03-29 14:32:42 -0500

    update network to mumbai testnet

--- hooks/useCommentsContract.ts
+++ hooks/useCommentsContract.ts
@@ -29,7 +29,7 @@ const useCommentsContract = () => {
   // We also pass in the signer if there is a signed in wallet, or if there's
   // no signed in wallet then we'll pass in the connected provider.
   const contract = wagmi.useContract({
-    addressOrName: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
+    addressOrName: '0x5618E1830C871B4fAD0B046D0fCEAcc5feA04b5e',
     contractInterface: CommentsContract.abi,
     signerOrProvider: signer.data || provider,
   })
